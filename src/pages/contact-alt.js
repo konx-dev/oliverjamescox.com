211f36ff91a3d925ce3a1b56c1de986fc244d59a
Oliver Cox <olivercoxdesign@gmail.com>
2018-05-29 22:47:23 +0100

    I broke it again..

--- src/pages/contact-alt.js
+++ src/pages/contact-alt.js
@@ -20,13 +20,13 @@ const ContactPageAlt = () => (
     >
  
   <p>
-    <label>Your Name: <input type="text" name="name" required /></label>   
+    <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" required /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
   </p>
   <p>
-    <label>Message: <textarea name="message" required ></textarea></label>
+    <label>Message: <textarea name="message" ></textarea></label>
   </p>
   <p>
     <button type="submit">Send</button>
