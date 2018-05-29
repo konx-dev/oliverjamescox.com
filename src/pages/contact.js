197249bfa42565a5e5f3df1fadcddc063d3d79f6
Oliver Cox <olivercoxdesign@gmail.com>
2018-05-29 21:40:35 +0100

    1 step forward, 2 steps back.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,50 +2,33 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const ContactPage = () => (
-  <div>
-    <div className="container">
-        <div className="wrapper">
-
-          <div className="my-info">
-            <h3>Contact Me</h3>
-            <p>Pop me a message if you would like to work on a project together, or just want to chat.</p>
-          </div>
-
-          <div className="contact">
-            
-            <form 
-              name="contact" 
-              method="POST" 
-              action="contact" 
-              data-netlify="true" 
-              data-netlify-honeypot="bot-field"
-            >
-                <input type="hidden" name="bot-field" />
-                <p>
-                  <label>Name 
-                    <input type="text" name="name" />
-                  </label>   
-                </p>
-                <p>
-                  <label>Email Address 
-                    <input type="email" name="email" placeholder="example@address.com" />
-                  </label>
-                </p>
-                <p>
-                  <label>Message 
-                    <textarea name="message" rows="6">
-                    </textarea>
-                  </label>
-                 </p>
-                  <p>
-                    <button type="submit">Send</button>
-                  </p>
-            </form>
-          </div>
-
-      </div>
+    <div>
+    <h1>Contact Me</h1>
+    <p>personal information page</p>
+    
+    <form 
+        name="my-contacts" 
+        method="POST" 
+        action="/thank-you" 
+        data-netlify="true" 
+        data-netlify-honeypot="bot-field"
+    >
+ 
+    <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+    </p>
+    <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+    <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+    <button type="submit">Send</button>
+    </p>
+    </form>
     </div>
-    </div>
+
 )
 
 export default ContactPage
