35755710472c20bf8d9dea74c36503430f93da10
olivercoxdesign <olivercoxdesign@gmail.com>
2018-05-31 00:48:14 +0100

    refactored contact form css to BEM

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,10 @@ import Link from 'gatsby-link'
 import Contactform from '../components/contact-form'
 
 const IndexPage = () => (
-  <div className="indexLayout">
-    <h1 className="indexH1">Hi, I’m Oliver Cox.</h1>
-    <p className="indexP">I’m a Digital Designer and Web Developer for Raleigh.</p>
+  <div>
+    <h1>Hi, I’m Oliver Cox.</h1>
+    <p>I’m a Digital Designer and Web Developer for Raleigh.</p>
+    
     <Contactform />
 
   </div>
