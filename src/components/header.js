006254dd405bce9c5f51f94752f836ae84b8be23
Oliver Cox <olivercoxdesign@gmail.com>
2018-06-04 21:39:03 +0100

    Fully Responsive Header & Navigation with CSS Grid / Flexbox

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import logo from '../images/oc_logo.svg'
 
 const Header = () => (
 
-  <div className="header--wrapper">
+/*  <div className="header--wrapper">
     <nav className="header--nav">
       <ul>
         <li><img className="logo" src={logo} alt="oc logo" /></li>
@@ -16,7 +16,27 @@ const Header = () => (
       </ul>
     </nav>
   </div>
-
+  
+  <div className="menu-logo">
+     <Link to="/"><img className="logo" src={logo} alt="oc logo" /></Link>
+    </div>
+  
+  */
+<div className="menu-container">
+  <div className="menu">
+    <div className="menu-logo">
+        <Link to="/"><img className="logo" src={logo} alt="oc logo" /></Link>
+    </div>
+    <nav className="menu-items">
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/work">Work</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+        </ul>
+    </nav>
+  </div>
+</div>
 )
 
 export default Header
