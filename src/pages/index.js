88fb9ca34de209c044f64ee4d8147b291cfae3e7
olivercoxdesign <olivercoxdesign@gmail.com>
2018-06-19 01:21:18 +0100

    added designer/developer breakdown & initial styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,84 @@ const IndexPage = () => (
       <div className="index__headline--sub">
         I design and code with modern tooling, and love what I do.
       </div>
+      <div className="index__keyinfo-container">
+        <div className="keyinfo-section">
+          Icon here
+          <div className="keyinfo-header">
+            Designer
+          </div>
+          <div className="keyinfo-body">
+            Quick Designer Overview goes here
+          </div>
+          <div className="keyinfo-subheader">
+          Skills
+          </div>
+          <div className="keyinfo-body">
+            UI Design, Branding, Editorial
+          </div>
+          <div className="keyinfo-subheader">
+          Tools
+          </div>
+          <div className="keyinfo-body">
+            <ul>
+              <li>Illustrator</li>
+              <li>InDesign</li>
+              <li>Photoshop</li>
+              <li>After Effects</li>
+              <li>Figma</li>
+            </ul>
+          </div>
+        </div>
+        <div className="keyinfo-section">
+          Icon here
+          <div className="keyinfo-header">
+            Developer
+          </div>
+          <div className="keyinfo-body">
+            Quick Developer Overview goes here
+          </div>
+          <div className="keyinfo-subheader">
+            Industry Knowledge
+          </div>
+          <div className="keyinfo-body">
+            Responsive Design, SEO, Email Marketing
+          </div>
+          <div className="keyinfo-subsection">
+            <div className="keyinfo-subheader">
+              Tools &amp; Tech
+              <div className="keyinfo-body">
+                <ul>
+                  <li>JavaScript</li>
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                  <li>Wordpress</li>
+                  <li>Node.js</li>
+                  <li>GraphQL</li>
+                </ul>
+              </div>
+            </div>
+            <div className="keyinfo-subheader">
+              Frameworks
+              <div className="keyinfo-body">
+              <ul>
+                <li>React</li>
+                <li>Angular</li>
+                <li>Express.js</li>
+                <li>Gatsby.js</li>
+                <li>Apollo</li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          <div className="keyinfo-subheader">
+            FWIW
+          </div>
+          <div className="keyinfo-body">
+            jQuery, SQL, Git, Gulp
+          </div>
+
+        </div>
+      </div>
       <div className="work-grid">
         <div className="work-item item_1">
           <Link to="/raleigh-history"><div className="work-content">
