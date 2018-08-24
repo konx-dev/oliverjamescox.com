29af72c6e2e6eda0d1ad2368a84f95497cce7eb5
Oliver Cox <olivercoxdesign@gmail.com>
2018-08-24 23:24:22 +0100

    refactored footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 import githubIcon from '../images/social-icons/github.svg'
 import cpenIcon from '../images/social-icons/codepen.svg'
@@ -6,14 +7,24 @@ import instaIcon from '../images/social-icons/instagram.svg'
 import twitIcon from '../images/social-icons/twitter.svg'
 import emailIcon from '../images/social-icons/email.svg'
 import linkedIcon from '../images/social-icons/linkedin.svg'
+import dribbbleIcon from '../images/social-icons/dribbble.svg'
 
 const Footer = () => (
 
   <div className="footer--wrapper">
+    <nav className="footer--nav">
+          <ul>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/portfolio">Portfolio</Link></li>
+              <li><Link to="/">Blog</Link></li>
+              <li><Link to="/">Contact</Link></li>
+          </ul>
+      </nav>
     <div className="footer--container">
       <ul>
         <li><a href="https://github.com/olivercoxdesign" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={githubIcon} alt="github logo" /></a></li>
         <li><a href="https://codepen.io/olivercoxdesign/" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={cpenIcon} alt="codepen logo" /></a></li>
+        <li><a href="https://dribbble.com/oliverjamescox" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={dribbbleIcon} alt="dribbble logo" /></a></li>
         <li><a href="https://www.instagram.com/ollycox86/?hl=en" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={instaIcon} alt="instagram logo" /></a></li>
         <li><a href="https://twitter.com/oliverjamescox" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={twitIcon} alt="twitter logo" /></a></li>
         <li><a href="mailto:olivercoxdesign@gmail.com"><img className="social-icon" src={emailIcon} alt="email logo" /></a></li>
