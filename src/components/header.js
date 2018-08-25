c88e02b9a9f22e3cff64394b06e6daf76a41f5a2
Oliver Cox <olivercoxdesign@gmail.com>
2018-08-25 12:35:37 +0100

    header rework

--- src/components/header.js
+++ src/components/header.js
@@ -12,10 +12,10 @@ const Header = () => (
     </div>
     <nav className="menu-items">
         <ul>
-            <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/portfolio">Portfolio</Link></li>
             <li><Link to="/">Blog</Link></li>
+            <li><Link to="/">Contact</Link></li>
         </ul>
     </nav>
   </div>
