eb1c43b64fc1fd119deee8b71ad4a7822dd1c278
Oliver Cox <olivercoxdesign@gmail.com>
2018-08-26 20:05:01 +0100

    About Page - rework

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,64 +1,94 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-import myCV from '../files/OliverCox_CV_Jun18.pdf'
-import myPortfolio from '../files/OliverCox_portfolio_Jun18.pdf'
+import Img from 'gatsby-image'
+import { graphql } from "gatsby"
 
-const AboutPage = () => (
+const AboutPage = ({ data }) => (
   <Layout>
-    <div className="about__headline">
-      Hi, I'm Oliver.
+    <div className="about__header">
+      <div className="about__header-image">
+        <Img fluid={data.image1.childImageSharp.fluid} />
+      </div>
     </div>
     <div className="about__body">
-        Currently a Designer / Developer for Raleigh Bikes based in Nottingham. I specialize in traditional print and digital design, alongside front end development. I also do freelance design and development projects. I have a keen interest in Photography, VR tech and always looking for sources of inspiration from subjects I enjoy to continue learning.
+      <div className="about__body-container">
+        <div className="cv-body-main">
+          Currently a Designer for Raleigh, based in Nottingham. I specialize in traditional print and digital design, alongside freelancing development projects. With a interest in usability and experience design, I'm always looking for sources of inspiration to continue learning.
+        </div>
+        <div className="cv-header">
+          Qualifications
+        </div>
+        <div className="cv-body">
+          <ul>
+            <li>Received a Bachelors degree in Graphic Design at Nottingham Trent University.</li>
+            <li>Excellent organisation, self-motivation and problem-solving skills.</li>
+            <li>Creative, strong communication skills and working consistently within established timelines</li>
+            <li>Passionate about design, web development and bleeding edge tech.</li>
+          </ul>
+        </div>
+        <div className="cv-header">
+          Experience
+        </div>
+        <div className="cv-body">
+          <ul>
+            <li className="cv-title">Digital Designer, Raleigh UK
+              <div className="cv-duration">October '17 - Present</div>
+              <div className="cv-description">In the last year, I have taken on new responsibility and I now create, maintain and develop all of the production code for the outbound emails. Making sure the code is tested and with sufficient fall backs. The emails have a 250% increase year on year for engagement. I work on projects across all social channels as the sole creative resource on the team. That includes content such as digital artwork, illustrations, wire-framing and Wordpress pages.</div>
+            </li>
+            <li className="cv-title">Graphic Designer, Raleigh UK
+              <div className="cv-duration">October '16 - October '17</div>
+              <div className="cv-description">Major projects included collaborating on the Raleigh 2017 Catalogue, featuring over 10,000 products. The design and artworking of accessory packaging ranges and the styling of a range of bikes. The continuous distribution of our product range through printed and digital collateral.</div>
+            </li>
+            <li className="cv-title">Graphic Designer, Creative Design &amp; Print
+              <div className="cv-duration">November '14 - March '16</div>
+              <div className="cv-description">I worked with clients often from the inception to completion giving me a great insight into the client relations aspect of agency work and a full understanding of the design process at all stages. I became confident dealing directly with clients and liaising with external suppliers. As a digital printers with on our competitive quick turnaround time required me to be efficient and organised with many projects running at one time.</div>
+            </li>
+          </ul>
+        </div>
+        <div className="cv-header">
+          Tools &amp; Tech
+        </div>
+        <div className="cv-tools-title">
+            Design
+        </div>
+        <div className="cv-body">
+          Work extensively across the Adobe CC product range on a daily basis. <strong>Illustrator</strong> is my goto for anything vector based; such as svg icons, also for any illustration work I do. <strong>Photoshop</strong> for anything pixel based. <strong>InDesign</strong> I use alot at work creating print collateral and digital assets. <strong>After Effects</strong> is something I use on occasions for motion graphics and looking to incorporate in a future project.<br></br><br></br>My goto for prototyping / wireframing is <strong>Figma</strong>, I have experimented with alternatives but found this to tick the most boxes. The ability to share my designs and recieve instant feedback along with it being accessible across mac and windows has really streamlined my design process.  
+        </div>
+        <div className="cv-tools-title">
+            Development
+        </div>
+        <div className="cv-body">
+          Strong understanding of the fundamentals in <strong>HTML</strong>, <strong>CSS</strong> &amp; <strong>JavaScript</strong>. Specifically for creating layouts utilizing CSS Grid and flexbox. Recently for React projects I have started to use CSS-in-JS with <strong>Styled Components</strong>.<br></br><br></br>
+          Frameworks I reach for are <strong>ReactJS</strong>, <strong>NodeJS</strong> and <strong>ExpressJS</strong> in a MERN stack and smaller projects with <strong>GatsbyJS</strong> as part of the JAM stack with queried markdown generated from <strong>GraphQL</strong>. I organise my projects with git and have experience of <strong>jQuery</strong> and <strong>Wordpress</strong> theming and page builders.
+        </div>
+        <div className="cv-header">
+          Education
+        </div>
+        <div className="cv-body">
+        <ul>
+          <li>BA (Hons) Graphic Design</li>
+          <li>BTEC: National Diploma - Graphic Design</li>
+          <li>A Levels in Graphic Design, Computing &amp; Law</li>
+          <li>GCSES - 9 A-C's</li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div className="CV_container">
-      <div className="CV_header">CURRICULUM VITAE</div>
-        <div className="CV_subheader">Qualifications</div>
-          <div className="CV_body">
-            <ul>
-              <li>Received a Bachelors degree in Graphic Design at Nottingham Trent University.</li>
-              <li>Excellent organisation, self-motivation and problem-solving skills.</li>
-              <li>Creative, strong communication skills and consistently meets deadlines.</li>
-              <li>Passionate about web development and new technologies.</li>
-            </ul>
-          </div>
-        <div className="CV_subheader">Experience</div>
-          <div className="CV_exp">
-            <ul>
-              <li className="CV_title">Designer &amp; Front-end Developer
-                <div className="CV_duration">Raleigh UK, Oct '17 - Present</div>
-                <div className="CV_description">I create, maintain and develop all of the production code for the outbound emails. Making sure the code is tested and with sufficient fall backs. The emails have a 250% increase year on year for engagement. I work on projects across all social channels as the sole creative resource on the team. That includes content such as digital artwork, illustrations, wire-framing and Wordpress pages.</div>
-              </li>
-              <li className="CV_title">Graphic Designer
-                <div className="CV_duration">Raleigh UK, Oct '16 - Oct '17</div>
-                <div className="CV_description">Major projects included collaborating on the Raleigh 2017 Catalogue, featuring over 10,000 products. The design and artworking of accessory packaging ranges and the styling of a range of bikes. The continuous distribution of our product range through printed and digital collateral.</div>
-              </li>
-              <li className="CV_title">Graphic Designer
-                <div className="CV_duration">Creative Design &amp; Print, Nov '14 - Mar '16</div>
-                <div className="CV_description">I worked with clients often from the inception to completion giving me a great insight into the client relations aspect of agency work and a full understanding of the design process at all stages. I became confident dealing directly with clients and liaising with external suppliers. As a digital printers with on our competitive quick turnaround time required me to be efficient and organised with many projects running at one time.</div>
-              </li>
-            </ul>
-          </div>
-          <div className="CV_subheader">Education</div>
-            <div className="CV_body">
-              <ul>
-                <li>BA (Hons) Graphic Design</li>
-                <li>BTEC: National Diploma - Graphic Design</li>
-                <li>A Levels in Graphic Design, Computing &amp; Law</li>
-                <li>GCSES - 9 A-C's</li>
-              </ul>
-            </div>
-            <div className="CV_downloads">
-              Downloads
-              <div className="CV_btns">
-                <a href={myCV} download="OliverCox_CV_Jun18">Curriculum Vitae</a>
-                <a href={myPortfolio} download="OliverCox_portfolio_Jun18">Portfolio Samples</a>
-              </div>
-              <span>Last Updated: June 2018</span>
-            </div>
-          </div>
-    </Layout>
+    
+  </Layout>
 )
 
 export default AboutPage
+
+export const query = graphql`
+  query AboutQuery {
+    image1: file(relativePath: { regex: "/oc-profilepic.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
