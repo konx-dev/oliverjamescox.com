6d25697552b720c4100a6aff76430ef78d99d3ee
Oliver Cox <olivercoxdesign@gmail.com>
2018-08-27 17:28:01 +0100

    hero block created

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,28 @@ import React from 'react'
 import Layout from '../components/layout'
 
 import Contactform from '../components/contact-form'
+import Img from 'gatsby-image'
+import { graphql } from "gatsby"
 
-const ContactPage = () => (
+const ContactPage = ({ data }) => (
   <Layout>
     <div className="contact__body">
+      <div className="contact__hero">
+        <div className="contact__hero--title">
+          CONTACT
+        </div>
+        <div className="contact__hero--image">
+          <Img  fluid={data.image1.childImageSharp.fluid}
+                style={{
+                  position: "relative",
+                  left: 0,
+                  top: 0,
+                  width: "100%",
+                  height: "40vh"
+                }} 
+          />
+        </div>
+      </div>
       <Contactform />
     </div>
   </Layout>
@@ -13,3 +31,15 @@ const ContactPage = () => (
 )
 
 export default ContactPage
+
+export const query = graphql`
+  query ContactQuery {
+    image1: file(relativePath: { regex: "/placeholder-4Kres-1.jpg/" }) {
+      childImageSharp {
+        fluid(  maxWidth: 1600 ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
