a6436fe8c99b66f586ec3f1c048a9b5261bbb174
Oliver Cox <olivercoxdesign@gmail.com>
2019-03-08 00:35:42 +0000

    0703 - End of Day Commit -- Added scroll to top functionality for blog posts & minor fixes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import ScrollToTop from '../components/scrollToTop'
 
 export default ({ data }) => {
-  
   const post = data.markdownRemark
 
   return (
 
+
     <Layout>
+        <ScrollToTop />
         <div className="body--background">
         <div className="blog--post-heroblock">
           <img className="blog--post-hero" src={post.frontmatter.thumbnail} alt={post.frontmatter.alt}/>
@@ -19,12 +21,23 @@ export default ({ data }) => {
               <h1 className="blog--post-title">{post.frontmatter.title}</h1>
               <div className="blog--post-desc">{post.frontmatter.description}</div>
               <div className="blog--post-innerHTML" dangerouslySetInnerHTML={{ __html: post.html }} />
-              <div className="blog--card-cta"><Link to="/blog">&larr; back</Link></div>
             </div>
         </div>
+        {/* <BlogPost 
+                title={post.frontmatter.title}
+                tech={post.frontmatter.topic}
+                date={post.frontmatter.date}
+                descript1={post.frontmatter.description}
+                descript2=""
+                link=""
+                text={post.html}
+                image={post.frontmatter.thumbnail}
+                alt={post.frontmatter.alt}
+              /> */}
     </Layout>
   )
 }
+
 
 export const query = graphql`
   query($slug: String!) {
