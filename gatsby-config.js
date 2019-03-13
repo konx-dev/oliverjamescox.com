5ef42b6a4fb5d564c78404f8c807afc8ad23f92b
Oliver Cox <olivercoxdesign@gmail.com>
2019-03-13 01:28:00 +0000

    moved favicon to top level

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'standalone',
-        icon: 'src/files/oc_logo512.png',
+        icon: 'oc_logo512.png',
         include_favicon: true,
       },
     },
