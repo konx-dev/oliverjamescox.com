38e00c41e22f9779a05632cb869b339bba95d9b4
Oliver Cox <olivercox@Olivers-MacBook-Pro.local>
2019-05-12 23:32:55 +0100

    removed depreciated analytics.js in favour of gtag.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,27 +13,14 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-140024933-1",
+        // your google analytics tracking id
+        trackingId: `UA-140024933-1`,
         // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is optional
+        head: false,
+        // enable ip anonymization
         anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
       },
     },
     'gatsby-transformer-remark',
